d009df29fd310798b3c756d3afe14ab60113f9ea
Eveliis <eveliis.kallas@khk.ee>
2019-04-25 12:46:19 +0300

    04 Filter PHPUnit tests 02_02

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -1,22 +1,20 @@
 <?php
 namespace TDD\Test;
 require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
+
 use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
-class ReceiptTest extends TestCase
-{
-    public function setUp()
-    {  //lisame meetodi setUp
+
+class ReceiptTest extends TestCase {
+    public function setUp(){  //lisame meetodi setUp
         $this->Receipt = new Receipt(); // loome receipt objekti, mis on võrdne uue receipt objektiga
     }
 
-    public function tearDown()
-    {  //lisame meetodi tearDown, see tühistab receipt objekti, tagades, et PHP ei kannaks midagi ühest testikäigust teise.
+    public function tearDown(){  //lisame meetodi tearDown, see tühistab receipt objekti, tagades, et PHP ei kannaks midagi ühest testikäigust teise.
         unset($this->Receipt); //
     }
 
-    public function testTotal()
-    {
+    public function testTotal(){
         $input = [0, 2, 5, 8]; // antud arvud, mis liidetakse kokku
         $output = $this->Receipt->total($input); // output on siis võrdne selle arve summaga. See muudab testi rohkem isoleeritumaks
         $this->assertEquals(
@@ -31,7 +29,7 @@ class ReceiptTest extends TestCase
     {
         $inputAmount = 10.00; //loome sisendi, mis on 10 rahaühikut
         $taxInput = 0.10; // see on maksu %
-        $output = $this->Receipt->tax($inputAmount, $taxInput); // kutsume selle meetodi Receipt objektis välja.
+        $output = $this->Receipt->tax($inputAmount, $taxInput); // kutsume selle meetodi Receipt objektis välja, mis korrutab parameetrid.
         $this->assertEquals(
             1.00,
             $output,
@@ -40,3 +38,7 @@ class ReceiptTest extends TestCase
     }
 }
 
+
+// Kui teste on mitu, mis tähendab, et terminal ei jooksuta neid kiiresti läbi. Seega on vaja terminali
+//hoopis kirjutada $vendor\bin\phpunit tests\ ja ss täpsustada faili ehk ReceiptTest.php, mmis peaks võtma
+//ainult testid, mis on selles failis
